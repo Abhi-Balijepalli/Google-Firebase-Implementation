e0d6710f26ef20c9546dcc5326358faa64535528
Abhi Balijepalli <57305830+Abhi-Balijepalli@users.noreply.github.com>
2021-02-01 22:11:30 -0800

    Update firebase.config.js

--- AuthTest-pinterlist/src/firebase.config.js
+++ AuthTest-pinterlist/src/firebase.config.js
@@ -1,11 +1,11 @@
 export default {
-  apiKey: "AIzaSyC9Zzum1b7EAjcJt1koZMVXvJMcRbioZSw",
-  authDomain: "authtest-pinterlist.firebaseapp.com",
-  databaseURL: "https://authtest-pinterlist.firebaseio.com",
-  projectId: "authtest-pinterlist",
-  storageBucket: "authtest-pinterlist.appspot.com",
-  messagingSenderId: "390793392807",
-  appId: "1:390793392807:web:2ee11c57d5588296aee182",
-  measurementId: "G-5K2VG86G7N"
+  apiKey: "-",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
 };
